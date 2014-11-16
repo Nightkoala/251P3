42ffc23d0f291b0e4de41c218a9f1c1e025b0106
Derek Brown <djb3718@rit.edu>
2014-11-16 16:35:28 -0500

    Added files to eclipse

---
+++ CSCI251_project3/src/ConnectFour.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ * @author Owner
+ *
+ */
+public class ConnectFour {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
